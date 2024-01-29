768e74b5283c1de5fdb0f767752e3c71af5c0ed7
Xander180 <wilson.ramirez98@gmail.com>
2024-01-29 10:04:33 -0700

    First version

---
+++ src/main/java/com/wrc195/wrc195task/HelloController.java
@@ -0,0 +1,14 @@
+package com.wrc195.wrc195task;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
