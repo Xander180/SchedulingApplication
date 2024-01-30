93f76ca58e6cbb784d2bc00d7cad1d421bcf13e0
Xander180 <wilson.ramirez98@gmail.com>
2024-01-30 13:17:46 -0700

    Create Login menu GUI, configured LoginController skeleton

--- src/main/java/com/wrc195/wrc195task/LoginController.java
+++ src/main/java/com/wrc195/wrc195task/LoginController.java
@@ -1,14 +1,46 @@
 package com.wrc195.wrc195task;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
+    private Label currentLocaleLbl;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button exitBtn;
+
+    @FXML
+    private Label localeTxtLbl;
+
+    @FXML
+    private Button loginBtn;
+
+    @FXML
+    private Label passwordLbl;
+
+    @FXML
+    private TextField passwordTxt;
+
+    @FXML
+    private Label usernameLbl;
+
+    @FXML
+    private TextField usernameTxt;
+
+    @FXML
+    private Label welcomeLbl;
+
+    @FXML
+    void onActionExit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionLogIn(ActionEvent event) {
+
     }
 }
